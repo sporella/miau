8cefcae5efc1c67653445a9dc0f1b4323b00cc4a
sporella <sporella@uc.cl>
2020-07-29 19:44:30 -0400

    incluye función gatitos

---
+++ R/gatitos.R
@@ -0,0 +1,13 @@
+#' Una función espléndida que transforma todo en gatito
+#'
+#' @param x Cualquier objeto
+#'
+#' @return Un gatito
+#' @export
+#'
+#' @examples
+#' frutas <- c("manzana", "naranja", "pera")
+#' gatitos(frutas)
+gatitos <- function(x){
+  return(">^..^<")
+}
